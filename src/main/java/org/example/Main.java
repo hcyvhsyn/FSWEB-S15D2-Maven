636beaced76c2e0e56a61a357957e968a9958efc
Huseyn Hajiyev <huseynhajiyev0@gmail.com>
2025-01-27 00:51:47 +0300

    done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,23 @@
 package org.example;
 
+import org.example.entity.Priority;
+import org.example.entity.Status;
+import org.example.entity.Task;
+import org.example.entity.TaskData;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-    }
+
+
+
+    Set<Task> annsTasks = new HashSet<>();
+        Task taskAnn = new Task("work","dummy desc","ann",Status.IN_PROGRESS, Priority.HIGH );
+
+
+
+}
 }
---
+++ src/main/java/org/example/entity/TaskData.java
@@ -0,0 +1,76 @@
+package org.example.entity;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
+public class TaskData {
+    private Set<Task> annsTasks;
+    private Set<Task> bobsTasks;
+    private Set<Task> carolsTasks;
+    private Set<Task> unassignedTasks;
+
+
+
+
+    public Set<Task> getTasks(String name){
+        switch (name){
+            case "ann":
+                return annsTasks;
+            case "bobs":
+                return bobsTasks;
+            case "carols":
+                return carolsTasks;
+            case "all":
+                return getUnion(annsTasks,bobsTasks,carolsTasks);
+
+        }
+       return  new HashSet<>();
+    }
+
+
+    public Set<Task> getUnion(Set<Task>... sets) {
+        HashSet<Task> allTasks = new LinkedHashSet<>();
+        for(Set<Task> tasks: sets){
+            allTasks.addAll(tasks);
+        }
+        return allTasks;
+    }
+
+    public TaskData(Set<Task> annsTasks, Set<Task> bobsTasks, Set<Task> carolsTasks, Set<Task> unassignedTasks) {
+        this.annsTasks = annsTasks;
+        this.bobsTasks = bobsTasks;
+        this.carolsTasks = carolsTasks;
+        this.unassignedTasks = unassignedTasks;
+    }
+
+    public Set<Task> getIntersection(Set<Task> first, Set<Task> second){
+        Set<Task> intersection = new HashSet<>(first);
+        intersection.retainAll(second);
+        return intersection;
+    }
+
+    public Set<Task> getDifferences(Set<Task> first, Set<Task> second){
+        Set<Task> difference = new HashSet<>(first);
+        difference.removeAll(second);
+        return difference;
+    }
+
+    public Set<Task> getAnnsTasks() {
+        return annsTasks;
+    }
+
+    public Set<Task> getBobsTasks() {
+        return bobsTasks;
+    }
+
+    public Set<Task> getCarolsTasks() {
+        return carolsTasks;
+    }
+
+    public Set<Task> getUnassignedTasks() {
+        return unassignedTasks;
+    }
+}
